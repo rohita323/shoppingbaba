33df009bf0a835a6e53e65d940ed062a6a6c6727
thewayofknowing <thewayofknowing@gmail.com>
2014-02-21 02:02:58 +0530

    Notifications Working

--- data/merchants.js
+++ data/merchants.js
@@ -1,9 +1,13 @@
+var s = "";
 $(document).ready(function() {
 	console.log("in here");
     $.getJSON('merchants.json', function(data) {
-        console.log("read");
 	    $.each(data.Websites, function(key, val) {
-	   	console.log(val.url);
-	});
+	    	s += val.url + ",";
+	       console.log(val.url);
+		});
+	console.log(s);
+    self.port.emit("url_list",s);
    });
+    
  });
--- data/notifications.js
+++ data/notifications.js
@@ -1,21 +1,12 @@
 
 
 var cart_image= document.getElementById("cart");
-var red_circle = document.getElementById("notification");
+var alert = document.getElementById("notification");
 var notif_value = document.getElementById("number");
 
-
-self.port.on("not_number", function(number) {
-	console.log(number);
-  if(number==0)
-  {
-  	red_circle.src=("empty.png");
-  	notif_value.innerHTML="";
-  }
-
-  else
-  {
-  	red_circle.src=("circle.png");
-  	notif_value.innerHTML=number;
-  }
+self.port.on("reset", function() {
+  alert.src = "empty.png";
+})
+self.port.on("BINGO", function() {             //BINGO sent when match found
+	alert.src = "alert.gif";
 });
